b7be63aa2bdac91d159f749ccfa379d1d06cc80a
Mustafa DİNÇ <mustafa9650@hotmail.com>
2022-02-18 02:27:27 +0300

    Rover Method Changed

--- MarsRover.Console/Program.cs
+++ MarsRover.Console/Program.cs
@@ -1,26 +1,13 @@
 ﻿
 
-
-/// İlk input  AreaSize Örn: 5 5 
-/// S-E-W-N yönleri için bir değişken tut
-/// hareket özellikler LMR 
-/// Maksimum alanı geçer ise hata mesajı gönder. 
-/// 
-/// 1. Plato Size 5,5 
-/// 2. Current Location 
-/// 3. LMLMLMLMLMMM
-/// 4. Verilen son yönden sonra son konumu 
-///     
-///Test için olması gerekenler 
-///1. sola ve sağa dönmeli 
-///ileri gidebilmeli 
-///area içerisinde kalmalı 
 /// 
 Console.WriteLine("Lütfen alan boyutunu yazınız. Örn: 5 5 ");
 var Area = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList();
-var CurrentLocation = Console.ReadLine().Trim().Split(' ');
-Rover rover = new Rover();
 
+Console.WriteLine("Şuanki konumu giriniz. Örn: 1 2 N ");
+List<string> CurrentLocation = Console.ReadLine().Trim().Split(' ').ToList();
+
+Rover rover = new Rover();
 if (CurrentLocation.Count() == 3)
 {
     rover.X = Convert.ToInt32(CurrentLocation[0]);
@@ -28,6 +15,7 @@ if (CurrentLocation.Count() == 3)
     rover.Direction = (Direction)Enum.Parse(typeof(Direction), CurrentLocation[2]);
 }
 
+Console.WriteLine("Rota giriniz. Örn: LMLMLMLMMM ");
 var Rotation = Console.ReadLine().ToUpper();
 
 try
--- MarsRover.Console/Services/Rover.cs
+++ MarsRover.Console/Services/Rover.cs
@@ -1,104 +1,106 @@
 ﻿
 
-    public class Rover : IRover
+public class Rover : IRover
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Direction Direction { get; set; }
+
+    public Rover()
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public Direction Direction { get; set; }
+        X = Y = 0;
+        Direction = Direction.N;
+    }
 
-        public Rover()
+    private void TurnLeft()
+    {
+        switch (this.Direction)
         {
-            X = Y = 0;
-            Direction = Direction.N;
-        }
-
-        private void TurnLeft()
-        {
-            switch (this.Direction)
-            {
-                case Direction.N:
-                    this.Direction = Direction.W;
-                    break;
-                case Direction.S:
-                    this.Direction = Direction.E;
-                    break;
-                case Direction.E:
-                    this.Direction = Direction.N;
-                    break;
-                case Direction.W:
-                    this.Direction = Direction.S;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        private void TurnRight()
-        {
-            switch (this.Direction)
-            {
-                case Direction.N:
-                    this.Direction = Direction.E;
-                    break;
-                case Direction.S:
-                    this.Direction = Direction.W;
-                    break;
-                case Direction.E:
-                    this.Direction = Direction.S;
-                    break;
-                case Direction.W:
-                    this.Direction = Direction.N;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        private void Forward()
-        {
-            switch (this.Direction)
-            {
-                case Direction.N:
-                    this.Y += 1;
-                    break;
-                case Direction.S:
-                    this.Y -= 1;
-                    break;
-                case Direction.E:
-                    this.X += 1;
-                    break;
-                case Direction.W:
-                    this.X -= 1;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public void Execute(List<int> Area, string Char)
-        {
-            foreach (var item in Char)
-            {
-                switch (item)
-                {
-                    case 'M':
-                        this.Forward();
-                        break;
-                    case 'L':
-                        this.TurnLeft();
-                        break;
-                    case 'R':
-                        this.TurnRight();
-                        break;
-                    default:
-                        Console.WriteLine($"Geçersiz Karakter {item}");
-                        break;
-                }
-
-                if (this.X < 0 || this.X > Area[0] || this.Y < 0 || this.Y > Area[1])
-                {
-                    throw new Exception($"İlgili alanın dışında:  ({Area[0]} , {Area[1]})");
-                }
-            }
+            case Direction.N:
+                this.Direction = Direction.W;
+                break;
+            case Direction.S:
+                this.Direction = Direction.E;
+                break;
+            case Direction.E:
+                this.Direction = Direction.N;
+                break;
+            case Direction.W:
+                this.Direction = Direction.S;
+                break;
+            default:
+                break;
         }
     }
+
+    private void TurnRight()
+    {
+        switch (this.Direction)
+        {
+            case Direction.N:
+                this.Direction = Direction.E;
+                break;
+            case Direction.S:
+                this.Direction = Direction.W;
+                break;
+            case Direction.E:
+                this.Direction = Direction.S;
+                break;
+            case Direction.W:
+                this.Direction = Direction.N;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Forward()
+    {
+        switch (this.Direction)
+        {
+            case Direction.N:
+                this.Y += 1;
+                break;
+            case Direction.S:
+                this.Y -= 1;
+                break;
+            case Direction.E:
+                this.X += 1;
+                break;
+            case Direction.W:
+                this.X -= 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void Execute(List<int> Area, string Char)
+    {
+        foreach (var item in Char)
+        {
+            switch (item)
+            {
+                case 'M':
+                    this.Forward();
+                    break;
+                case 'L':
+                    this.TurnLeft();
+                    break;
+                case 'R':
+                    this.TurnRight();
+                    break;
+                default:
+                    Console.WriteLine($"Geçersiz Karakter {item}");
+                    break;
+            }
+
+            if (this.X < 0 || this.X > Area[0] || this.Y < 0 || this.Y > Area[1])
+            {
+                throw new Exception($"Alanı geçtiniz :  ({Area[0]} , {Area[1]})");
+            }
+
+        }
+    }
+}
